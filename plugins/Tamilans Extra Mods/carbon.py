3176ff7f23c9405b1787b552d23020bc01c81308
nayaganfilterbo <140459247+nayaganfilterbo@users.noreply.github.com>
2023-08-31 23:06:32 +0530

    Update carbon.py

--- plugins/Tamilans Extra Mods/carbon.py
+++ plugins/Tamilans Extra Mods/carbon.py
@@ -42,8 +42,8 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**CᴏᴅᴇᴅᴇD Bʏ @Tamilan_BotsZ**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**CᴏᴅᴇᴅᴇD Bʏ @Nayaganbot**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="http://t.me/Nayagan_Update_bot")]]),                   
     )
     await m.delete()
     carbon.close()
